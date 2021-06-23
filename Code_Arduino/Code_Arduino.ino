c8aa1e01023259a0a24214d1a8d3744bd5b76b0d
Julian <69975747+DevTheJul@users.noreply.github.com>
2021-06-23 18:52:16 +0200

    Add files via upload

--- Code_Arduino/Code_Arduino.ino
+++ Code_Arduino/Code_Arduino.ino
@@ -2,11 +2,41 @@
 #include <MFRC522.h>
 
 /**
-Definiton der Pins für den Piezo-Buzzer und den NFC-Scanner
+Code von David Traum überarbeitet und ergänzt von Julian Rheker
 */
+
+/**
+Code zum Auslesen des RFID:
+void loop() 
+{
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  //Show UID on serial monitor
+  Serial.print("UID tag :");
+  String content= "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  }
+
+*/
+
 #define BUZZER D2
-#define SS_PIN D4
-#define RST_PIN D3
+#define SS_PIN D4 // rfid pin
+#define RST_PIN D3 // rfid pin
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 
 
@@ -300,7 +330,7 @@ void calculateFazit() {
   Serial.print(earths);
   Serial.print(".pic=10");
   nexSend();
- }
+ }//fertig
  Serial.print("t_value.txt=\"");
  Serial.print((total/1000.0));
  Serial.print(" Tonnen CO2\"");
